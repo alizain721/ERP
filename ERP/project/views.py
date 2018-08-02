b42ff6c9e5da09f0f2201dfec1685b18d8ed5045
alizain721 <alizain721@gmail.com>
2018-08-02 18:42:29 +0500

    Create ApiView for project get request
    Creatd ApiView class for project and handled 2 types of get request
    1) Fetch  all projects
    2) Fetch a particular project,provied project_id as parameter

--- ERP/project/views.py
+++ ERP/project/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from .models import Project
 
-# Create your views here.
+class ProjectApiView(APIView):
+    """Project APIview to handul all restfull requests"""
+    def get(self,request, format=None):
+        """This method will return either all projects or a signle proect"""
+        project_id = request.GET.get('project_id')
+        if project_id:
+            project = Project.objects.filter(id=project_id)
+            project_serializer = ProjectSerializer(project, many=True)
+        else:
+            projects = Project.objects.all()
+            project_serializer = ProjectSerializer(projects, many=True)
+        return Response({"data":project_serializer.data})
+
