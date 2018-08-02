399880e67b9912fc1faa8ad3466f7e73887eb511
alizain721 <alizain721@gmail.com>
2018-08-02 15:57:01 +0500

    Make postgre as database of application
    Replaced sqlite3 with postgre as database

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'erp',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
