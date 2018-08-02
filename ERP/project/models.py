da92339b6ad7dfc0d043bbe09db52970ccc0f0bb
alizain721 <alizain721@gmail.com>
2018-08-02 16:27:11 +0500

    Create project model
    Project model created for api

--- ERP/project/models.py
+++ ERP/project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    """Project model class"""
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
