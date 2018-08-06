5c9063f2c1624e4a6c77bc7332e3def9df4590c7
alizain721 <alizain721@gmail.com>
2018-08-06 12:57:39 +0500

    Create loghours model
    Created loghours model to maintain loghours of employees

--- ERP/loghours/models.py
+++ ERP/loghours/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from dashboard.models import *
+from project.models import *
 
-# Create your models here.
+
+class Loghours(models.Model):
+    """ Loghours model class"""
+    date = models.DateField(null=True, blank=True)
+    hours_logged = models.FloatField(null=True)
+    required_hours = models.FloatField(null=True)
+    pm_approval = models.BooleanField()
+    am_approval = models.BooleanField()
+    hourly_rate = models.FloatField(null=True)
+    is_valid = models.BooleanField()
+    employee = models.ForeignKey(User,blank=True,on_delete=models.DO_NOTHING )
+    project = models.ForeignKey(Project,on_delete=models.DO_NOTHING)
