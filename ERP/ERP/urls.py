c6ac7721edb71917b8e38994a4e9450c2645376b
Abdul Sami <abdul.sami@tintash.com>
2018-08-06 13:12:15 +0500

    Add REST Framework's urls
    Add REST framework's urls for authentication in urls.py.

--- ERP/ERP/urls.py
+++ ERP/ERP/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup', include('dashboard.urls')),
+    path('signup/', include('dashboard.urls')),
+    path('auth/', include('rest_framework_social_oauth2.urls'))
 ]
