6689626973f37fb15f306dfcc435795864322fdd
alizain721 <alizain721@gmail.com>
2018-08-06 14:25:31 +0500

    Crete api to get user loghours
    In this commit api is creatd to get user loghours based upon parameters passed,there are following options of parameters
    1) user_id and project_id
    2) project_id

--- ERP/loghours/views.py
+++ ERP/loghours/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Loghours
+from .serializers import LogHoursSerializer
 
-# Create your views here.
+class LogHoursApiView(APIView):
+    """ApiView for loghours"""
+    def get(self,request, format=None):
+        """This method will return logohours of an employee given user_id and project_id as parameter"""
+        project_id = request.GET.get('project_id')
+        user_id = request.GET.get('user_id')
+        if project_id and user_id:
+            logohours = Loghours.objects.filter(project=project_id, employee_id=user_id)
+            if logohours:
+                loghours_serializer = LogHoursSerializer(logohours, many=True)
+            else:
+                return Response({"message":"No record found"},status = status.HTTP_404_NOT_FOUND)
+        elif project_id:
+            logohours = Loghours.objects.filter(project=project_id)
+            if logohours:
+                loghours_serializer = LogHoursSerializer(logohours, many=True)
+            else:
+                return Response({"message":"No record found"},status = status.HTTP_404_NOT_FOUND)
+        else:
+            logohours = Loghours.objects.all()
+            loghours_serializer = LogHoursSerializer(logohours, many=True)
+        return Response({"data":loghours_serializer.data})
