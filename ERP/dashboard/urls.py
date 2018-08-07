68cfec2ca4f7975b606d6c0ded084bac995226ba
alizain721 <alizain721@gmail.com>
2018-08-07 14:04:30 +0500

    Create api end points for pm to accept or reject employee request to be added or removed from any project
    I have created 2 apis one for accepting employee request that will take request_id as parameter and other for rejecting employee request that will take request_id and reason for rejecting the request as the parameter

--- ERP/dashboard/urls.py
+++ ERP/dashboard/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('user_projects',views.user_projects),
     path('employee_project_request',views.employee_project_request),
     path('employee_requests_all',views.employee_requests_all),
+    path('accept_request',views.accept_request),
+    path('reject_request',views.reject_request),
 ]
--- ERP/dashboard/views.py
+++ ERP/dashboard/views.py
@@ -67,3 +67,33 @@ def employee_requests_all(request):
     user_requests = UserProjectRequest.objects.filter(employee=user_id)
     user_project_request_serializer = UserProjectRequestSerializer(user_requests, many=True)
     return Response({"user_requests":user_project_request_serializer.data},status=status.HTTP_200_OK)
+
+@csrf_exempt
+@api_view(['POST'])
+def accept_request(request):
+    """ This method or api is for pm to accept request for employees to be added to project"""
+    data = request.data
+    request_id = data["request_id"]
+    try:
+        request = UserProjectRequest.objects.get(id=request_id)
+        request.status = "accpted"
+        request.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except UserProjectRequest.DoesNotExist:
+        return Response({"message": "Record not found"},status=status.HTTP_404_NOT_FOUND)
+
+@csrf_exempt
+@api_view(['POST'])
+def reject_request(request):
+    """ This method or api is for pm to reject request for employees to be added to project, it takes request_id and reason as input"""
+    data = request.data
+    request_id = data["request_id"]
+    reason = data['reason']
+    try:
+        user_project_request = UserProjectRequest.objects.get(id=request_id)
+        user_project_request.status = "rejected"
+        user_project_request.reason = reason
+        user_project_request.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except UserProjectRequest.DoesNotExist:
+        return Response({"message": "Record not found"},status=status.HTTP_404_NOT_FOUND)
